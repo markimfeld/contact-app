ac2b1ceabf5f2d650263b6584f70214ac62153b8
marcos <sebastianimfeld@gmail.com>
2020-04-28 23:28:11 -0300

    created contacts app

---
+++ contacts/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=100)
+    phone_number = models.IntegerField()
+    email = models.EmailField()
+    job = models.CharField(max_length=100)
+    website = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    avatar = models.ImageField()
+    created = models.DateField()
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.first_name
+
+
+class PhoneNumber(models.Model):
+    number = models.IntegerField()
