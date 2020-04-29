871ad924e70b81031df2986fb7d88d7760b039c0
marcos <sebastianimfeld@gmail.com>
2020-04-29 15:48:10 -0300

    navbar

--- contacts/views.py
+++ contacts/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world!")
+    return render(request, 'contacts/index.html')
+
+
+def login(request):
+    pass
+
+def register(request):
+    pass
